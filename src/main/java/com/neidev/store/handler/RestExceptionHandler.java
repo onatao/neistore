ebe97a7b526a00cf0c2adfbf3022200b300fdad2
Nathan <nathanbarrosnt@gmail.com>
2023-08-28 11:14:07 -0300

    fix: BuyerService exception handling solved

--- src/main/java/com/neidev/store/handler/RestExceptionHandler.java
+++ src/main/java/com/neidev/store/handler/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.neidev.store.handler;
 
 import java.util.Date;
 
+import com.neidev.store.handler.exceptions.CredentialAlreadyInUseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -34,5 +35,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 				request.getDescription(false)
 			);
 		return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(CredentialAlreadyInUseException.class)
+	public final ResponseEntity<ExceptionResponse> handlerConflictExceptions(Exception e, WebRequest request) {
+		ExceptionResponse exception = new ExceptionResponse(
+			new Date(),
+				e.getMessage(),
+				request.getDescription(false)
+		);
+		return new ResponseEntity<>(
+				exception, HttpStatus.CONFLICT
+		);
 	}
 }
